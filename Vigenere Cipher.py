31bb123501f969b5ae6b6fa0b739704f9fa20d01
Sebastian Segura <89867337+SebastianGautamSegura@users.noreply.github.com>
2022-05-30 16:05:20 -0400

    Add files via upload

---
+++ Vigenere Cipher.py
@@ -0,0 +1,106 @@
+# A basic Vigenere Cipher, meant to encrypt messages with a key
+'''
+This program uses a global letter matrix to encrypt messages. The program functions like a basic vigenere cipher
+with no special modifications.
+Users can either encrypt or decrypt messages.
+A helper function that modifies the key is used as well, but should not be accessed by the user.
+'''
+LETTER_MATRIX = [["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+                 ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a"],
+                 ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b"],
+                 ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"],
+                 ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d"],
+                 ["f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e"],
+                 ["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f"],
+                 ["h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g"],
+                 ["i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h"],
+                 ["j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i"],
+                 ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+                 ["l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"],
+                 ["m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"],
+                 ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"],
+                 ["o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"],
+                 ["p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"],
+                 ["q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"],
+                 ["r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"],
+                 ["s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"],
+                 ["t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"],
+                 ["u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"],
+                 ["v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"],
+                 ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"],
+                 ["x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w"],
+                 ["y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x"],
+                 ["z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"]
+                 ]
+
+
+def encrypt():
+    message = input("Enter message: ")
+    key = input("Enter key: ")
+
+    message = message.lower()
+    key = key.lower()
+    message_size = len(message)
+
+    adjusted_key = modify_key(key, len(key), message_size)
+
+    enc_message = ""
+
+    for i in range(len(message)):
+        if message[i] == " ":
+            enc_message = enc_message + "%"
+        else:
+            x_coord = ord(message[i]) - 97
+            y_coord = ord(adjusted_key[i]) - 97
+            enc_message = enc_message + LETTER_MATRIX[x_coord][y_coord]
+
+    return enc_message
+
+
+def decrypt():
+    message = input("Enter encrypted message: ")
+    key = input("Enter key: ")
+
+    key = key.lower()
+    message_size = len(message)
+
+    adjusted_key = modify_key(key, len(key), message_size)
+    dec_message = ""
+
+    for i in range(len(adjusted_key)):
+        if message[i] == "%":
+            dec_message = dec_message + " "
+        else:
+            y_coord = ord(adjusted_key[i]) - 97
+
+            x_coord = 0
+            for j in range(len(LETTER_MATRIX[y_coord])):
+                if LETTER_MATRIX[y_coord][j] == message[i]:
+                    x_coord = ord(LETTER_MATRIX[0][j]) - 97
+                    break
+
+            dec_message = dec_message + LETTER_MATRIX[x_coord][0]
+
+    return dec_message
+
+
+def modify_key(key, key_size, message_size):
+    if key_size >= message_size:
+        key = key[:message_size]
+        return key
+    else:
+        full_key = message_size // key_size
+        sub_key = message_size % key_size
+        new_key = ""
+        for i in range(full_key):
+            new_key = new_key + key
+
+        for i in range(sub_key):
+            new_key = new_key + key[i]
+
+        return new_key
+
+
+print(encrypt())
+print(decrypt())
+
